be4183e257ca2bfab5371a6107210633b91da9f1
Tobi Isaac <tobiisaac428@gmail.com>
2024-07-10 08:48:35 +0100

    toggle on small screen

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import shop from "../../assets/images/shop.png";
-import heart from "../../assets/images/heart.png"
+import heart from "../../assets/images/heart.png";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-
-   
-      <div className="nav">
-        <div>
-          <h3>Timbu Shop</h3>
-        </div>
-        <div className="img">
-          <img src={heart} alt="heart-icon" className="heart-icon" />
-          <img src={shop} alt="shop-icon" className="shop-icon" />
-        </div>
+    <div className="nav">
+      <div>
+        <h3>Timbu Shop</h3>
       </div>
-      
-
+      <div className={`img ${isMenuOpen ? "active" : ""}`}>
+        <img src={heart} alt="heart-icon" className="heart-icon" />
+        <img src={shop} alt="shop-icon" className="shop-icon" />
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="toggle-icon">&#9776;</span>
+      </div>
+    </div>
   );
 };
 
